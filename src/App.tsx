8da863e2c791c0d99e996d08f37edb7549e97498
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-08-14 19:50:27 -0700

    Images for our genere aside bar

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" >
+        <GridItem area="aside" paddingX={5}>
         <GenreList></GenreList>
         </GridItem>
       </Show>
